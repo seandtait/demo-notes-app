264445965434a6c199fb201c8ea785341fd829ff
seandtait <seandontait@gmail.com>
2022-06-10 23:05:07 +0100

    Adding the API

---
+++ backend/functions/create.js
@@ -0,0 +1,25 @@
+import * as uuid from "uuid";
+import handler from "../util/handler";
+import dynamoDb from "../util/dynamodb";
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+export const main = handler(async (event) => {
+    // Request body is passed in as a JSON encoded string in 'event.body'
+    const data = JSON.parse(event.body);
+    const params = {
+        TableName: process.env.TABLE_NAME,
+        Item: {
+            // The attributes of the item to be created
+            userId: "123", // The id of the author
+            noteId: uuid.v1(), // A unique uuid
+            content: data.content, // Parsed from request body
+            attachment: data.attachment, // Parsed from request body
+            createdAt: Date.now(), // Current unix timestamp
+        },
+    };
+
+    await dynamoDb.put(params);
+
+    return params.Item;
+});
