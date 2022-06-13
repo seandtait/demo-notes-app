18ce855e37ff62a569c1264b832523e3fc913b0d
seandtait <seandontait@gmail.com>
2022-06-13 22:34:31 +0100

    Setting up our React app

--- stacks/index.js
+++ stacks/index.js
@@ -1,14 +1,15 @@
 import { StorageStack } from "./StorageStack";
 import { ApiStack } from "./ApiStack";
 import { AuthStack } from "./AuthStack";
+import { FrontendStack } from "./FrontendStack";
 
 export default function main(app) {
-  app.setDefaultFunctionProps({
-    runtime: "nodejs16.x",
-    srcPath: "backend",
-    bundle: {
-      format: "esm",
-    },
-  });
-  app.stack(StorageStack).stack(ApiStack).stack(AuthStack);
+    app.setDefaultFunctionProps({
+        runtime: "nodejs16.x",
+        srcPath: "backend",
+        bundle: {
+            format: "esm",
+        },
+    });
+    app.stack(StorageStack).stack(ApiStack).stack(AuthStack).stack(FrontendStack);
 }
